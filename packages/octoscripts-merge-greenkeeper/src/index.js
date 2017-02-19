4cc5af294bdea807eca2f64276073c961cdb9ed7
Thomas Sileghem <th.sileghem@gmail.com>
2017-02-19 18:18:20 +0000

    fix(merge-greenkeeper): listen only to status

--- packages/octoscripts-merge-greenkeeper/src/index.js
+++ packages/octoscripts-merge-greenkeeper/src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = async ({isGithubWebhook, eventType, event, repository}) => {
-    if (!isGithubWebhook && eventType !== 'status') {
+    if (!isGithubWebhook || eventType !== 'status') {
         return;
     }
 
